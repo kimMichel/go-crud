f157fb6759b98b89ccc6f4ae0dc0bbfead9dd4f8
kim <kim@xds.com.br>
2023-04-14 17:20:26 -0300

    configure connection with database

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/kimMichel/go-crud/database"
+)
+
+func init() {
+	database.ConnectDB()
+}
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"message": "Hello, Kim!"})
+		return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
 	})
 
 	app.Listen(":3000")
